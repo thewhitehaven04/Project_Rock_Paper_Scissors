8bda8fc6e5a5af72c398e94524ed1f66bf6a5b35
thewhitehaven04 <thewhitehaven04@gmail.com>
2021-10-15 01:05:36 +0300

    finished rework of the logic

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
+const rock = { option: document.getElementById("rock") };
+const paper = { option: document.getElementById("paper") };
+const scissors = { option: document.getElementById("scissors") };
 
-rock = { option: rock, winsAgainst: scissors, losesAgainst: paper };
-paper = { option: paper, winsAgainst: rock, losesAgainst: scissors };
-scissors = { option: scissors, winsAgainst: paper, losesAgainst: rock };
+Object.defineProperty(rock, "winsAgainst", { value: scissors });
+Object.defineProperty(paper, "winsAgainst", { value: rock });
+Object.defineProperty(scissors, "winsAgainst", { value: paper });
 
 const optionElementMap = { rock, paper, scissors };
 
@@ -24,69 +24,54 @@ function playerPlay(target) {
 
 function playRound(playerSelection, computerSelection) {
   // Returns the outcome of a round.
-  if (playerSelection === "rock") {
-    switch (computerSelection) {
-      case paper:
-        result = "computer";
-        break;
-      case scissors:
-        result = "player";
-        break;
-      case rock:
-        result = "draw";
-        break;
-    }
+  let loser, winner;
+  let outcome = { loser: loser, winner: winner, draw: false };
+
+  if (playerSelection.winsAgainst === computerSelection) {
+    loser = computerSelection;
+    winner = playerSelection;
+  } else if (playerSelection.losesAgainst === computerSelection) {
+    loser = playerSelection;
+    winner = computerSelection;
+  } else {
+    outcome['draw'] = playerSelection;
   }
-
-  if (playerSelection === "paper") {
-    switch (computerSelection) {
-      case "scissors":
-        result = "computer";
-        break;
-      case "rock":
-        result = "player";
-        break;
-      case "paper":
-        result = "draw";
-        break;
-    }
-  }
-
-  if (playerSelection === "scissors") {
-    switch (computerSelection) {
-      case "rock":
-        result = "computer";
-        break;
-      case "paper":
-        result = "player";
-        break;
-      case "scissors":
-        result = "draw";
-        break;
-    }
-  }
-  return result;
-}
-
-function buttonListener(e) {
-  cleanResult();
-  computerSelection = computerPlay();
-  playerSelection = playerPlay(e.target);
-
-  result = playRound(playerSelection, computerSelection);
-  outputResult(result);
+  return outcome;
 }
 
 function addButtonListeners() {
   buttons = document.querySelectorAll(".controls__button");
-  buttons.forEach((button) => button.addEventListener("click", buttonListener));
+  buttons.forEach((button) =>
+    button.addEventListener(
+      "click",
+      (buttomImageListener = (e) => {
+        cleanResult();
+
+        computerSelection = computerPlay();
+        playerSelection = playerPlay(e.target);
+
+        result = playRound(playerSelection, computerSelection);
+        outputResult(result);
+      })
+    )
+  );
 }
 
-function outputResult(result) {}
+function outputResult(result) {
+  if (result['loser']) result['loser']['option'].classList.add("loss");
+  if (result['winner']) result['winner']['option'].classList.add("win");
+  if (result['draw']) result['draw']['option'].classList.add("draw");
+}
 
 function cleanResult() {
   resultField = document.querySelector(".result-bar__text");
   resultField.textContent = "RESULT: ";
+
+  Object.values(optionElementMap).forEach((element) => {
+    element.option.classList.remove("loss");
+    element.option.classList.remove("win");
+    element.option.classList.remove("draw");
+  });
 }
 
 addButtonListeners();
